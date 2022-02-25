a0e3e8510baa33fb5c566a4eba93420c40080ff2
huynguyen294 <anhhuy2099@gmail.com>
2022-02-25 14:33:35 +0700

    update F8_node_js

--- F8_node_js/get_started/index.js
+++ F8_node_js/get_started/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const { compile } = require('morgan')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
+
+app.use(morgan('combined'))
 
 app.get('/', (req, res) => {
     var a = 1
