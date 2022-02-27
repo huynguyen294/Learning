1a29a53ed063fed1f5c7f2432fc45ee634498de4
Nguyễn Hoàng Huy <73062886+huynguyen294@users.noreply.github.com>
2022-02-27 14:02:31 +0700

    Delete 50_project_web directory

--- 50_project_web/12_FQA_Collapse/script.js
+++
@@ -1,9 +0,0 @@
-const faqToggles = document.querySelectorAll('.faq-toggle')
-
-faqToggles.forEach((toggle) => {
-    toggle.addEventListener('click', () => {
-        toggle.parentNode.classList.toggle('active')
-        /* const subMenu = document.querySelectorAll(`${toggle.parentNode} .sub-menu`) */
-    })
-})
-
