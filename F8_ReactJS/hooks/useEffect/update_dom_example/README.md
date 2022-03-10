0b7b6d082ad0b03026aeee29ee93d1fcdf292be1
huynguyen294 <anhhuy2099@gmail.com>
2022-03-10 11:40:04 +0700

    add React useEffect with deps

---
+++ F8_ReactJS/hooks/useEffect/update_dom_example/README.md
@@ -0,0 +1,1 @@
+Đây là ứng dụng thay đổi title của trang sử dụng useEffect()
---
+++ F8_ReactJS/hooks/useEffect/with_deps/src/Content.js
@@ -0,0 +1,53 @@
+import {useState, useEffect} from 'react'
+
+function Content() {
+
+    const [title, setTitle] = useState('')
+    const [posts, setPosts] = useState([])
+    const [type, setType] = useState('posts')
+    const tabs = ['posts', 'comments', 'albums']
+
+    console.log('mounted')
+
+    useEffect(()=> {
+        document.title = title
+    }, [title])
+
+    useEffect(()=> {
+        fetch(`https://jsonplaceholder.typicode.com/${type}`)
+            .then(res => res.json())
+            .then(posts => setPosts(posts))
+    }, [type])
+
+    return (
+        <div className="content">
+            {
+                tabs.map((tab, idx) => 
+                    <button 
+                        key={idx}
+                        style={
+                            tab === type?
+                            {color: '#fff',
+                            backgroundColor: '#ccc'}
+                            :{}
+                        }
+                        onClick={()=>setType(tab)}
+                    >{tab}</button>
+                )
+            }
+            <input
+                value={title}
+                onChange={(e)=> setTitle(e.target.value)}
+            />
+            <ul>
+                {
+                    posts.map(post =>
+                        <li key={post.id}>{post.body?post.body:post.title}</li>
+                    )
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default Content
