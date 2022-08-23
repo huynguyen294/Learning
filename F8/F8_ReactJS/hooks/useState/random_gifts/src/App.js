2fe0066ab7c04a1bb5b5b59ea8ad230f4c5ee6f1
anhhuy294 <anhhuy2099@gmail.com>
2022-08-23 11:08:41 +0700

    nothing

--- F8/F8_ReactJS/hooks/useState/random_gifts/src/App.js
+++ F8/F8_ReactJS/hooks/useState/random_gifts/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react'
+import { useState } from 'react';
 
 //bài toán nhấn nút và lấy một phân thưởng bất kì
 
@@ -11,20 +11,20 @@ const gifts = [
   '100k',
   '500k',
   '1000k',
-]
+];
 
 function App() {
   // initSate để trống là undifine
-  const [gift, setGift] = useState()
+  const [gift, setGift] = useState();
 
-  const randomGift = ()=>{
-    const idx = Math.floor(Math.random()*gifts.length)
-    setGift(gifts[idx])
-  }
+  const randomGift = () => {
+    const idx = Math.floor(Math.random() * gifts.length);
+    setGift(gifts[idx]);
+  };
 
   return (
     <div className="App">
-      <h1>{ gift || 'Chưa có phần thưởng'}</h1>
+      <h1>{gift || 'Chưa có phần thưởng'}</h1>
       <button onClick={randomGift}>Lấy thưởng</button>
     </div>
   );
